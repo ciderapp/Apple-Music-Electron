3b5753acfd78106ee1a59fd8664a56702bc02682
Fire <iifir3z@protonmail.com>
2020-08-29 17:49:49 -0700

    Add brand new Discord Rich Presence.

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { app, BrowserWindow, globalShortcut } = require('electron')
 var path = require('path')
-
+const client = require('discord-rich-presence')('749317071145533440');
+var sgname
+var sgartist
 function createWindow () {
   // Create the browser window.
   const win = new BrowserWindow({
     icon: path.join(__dirname, './assets/icon.png'),
-    width: 800,
+    width: 1024,
     height: 600,
     minWidth: 300,
     minHeight: 300,
@@ -19,7 +21,7 @@ function createWindow () {
   win.setMenuBarVisibility(false);
 
   // load Apple Music site
-  win.loadURL("http://beta.music.apple.com");
+  win.loadURL("https://beta.music.apple.com");
 
   // hide iTunes prompt and other random bullshittery by Apple.
   win.webContents.on('did-frame-finish-load', function() {
@@ -31,7 +33,43 @@ function createWindow () {
     win.webContents.executeJavaScript("while (elements.length > 0) elements[0].remove();");
   });
 
+  // update rich presence when audio is playing.
+  win.webContents.on('media-started-playing', function() {
+    client.updatePresence({
+      state: "(Solo Listening)",
+      details: "Listening to Music!",
+      startTimestamp: Date.now(),
+      endTimestamp: Date.now() + 1337,
+      largeImageKey: 'apple',
+      smallImageKey: 'credit',
+      instance: true,
+    });
+  });
+
+  // Update rich presence when audio is paused or turned off.
+  win.webContents.on('media-paused', function() {
+    client.updatePresence({
+      state: "(Idling)",
+      details: "Music is paused.",
+      startTimestamp: Date.now(),
+      endTimestamp: Date.now() + 1337,
+      largeImageKey: 'apple',
+      smallImageKey: 'credit',
+      instance: true,
+    });
+  });
+
 }
+// Start rich presence service into idle mode.
+client.updatePresence({
+  state: 'Choosing music.',
+  details: '(Idle)',
+  startTimestamp: Date.now(),
+  endTimestamp: Date.now() + 1337,
+  largeImageKey: 'apple',
+  smallImageKey: 'credit',
+  instance: true,
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
