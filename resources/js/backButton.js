d5932fec36611566c4238235b6daed25305240c9
Core <64542347+coredev-uk@users.noreply.github.com>
2021-07-31 12:32:29 +0100

    Moved back button back to loading-inner because of users not using MacOS Emulation are experiening issues due to the search bar taking up the area.

--- resources/js/backButton.js
+++ resources/js/backButton.js
@@ -3,24 +3,12 @@ try {
     if (!document.querySelector('#backButton')) {
         const backButtonStyle = `background-color: rgb(0 0 0 / 0%); width: 9px; margin: -43px 8px 8px 228px; -webkit-app-region: no-drag;`;
 
-        document.getElementById('green').insertAdjacentHTML("afterend", `
+        document.getElementsByClassName('loading-inner')[0].insertAdjacentHTML("afterbegin", `
             <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
                 <img src="https://beta.music.apple.com/assets/shelf/paddle-dark.svg" alt="nope">
             </div>
         `);
     }
-
-    /* Keep this to remember the class and location for where we need to put it if we want it to look like mac
-    if (!document.querySelector('#backButton')) {
-      const backButtonStyle = `position: absolute; left: 0; float: left; border: 3px solid var(--searchBarBorderColor); background-color: rgba(60, 60, 67, 0.45); width: 20px`;
-
-      document.getElementsByClassName('loading-inner')[0].insertAdjacentHTML("afterbegin", `
-          <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
-              <p> < </p>
-          </div>
-      `);
-  }*/
-
 
 } catch (e) {
     console.error("[JS] Error while trying to apply backButton.js", e);
