ec179e68bb935afb6859031f2950489cd984277c
Core <64542347+coredev-uk@users.noreply.github.com>
2021-08-02 16:41:09 +0100

    Resolves issue in #148. Light version added and css adjusted for right positioning of back button.

--- resources/js/backButton.js
+++ resources/js/backButton.js
@@ -1,13 +1,22 @@
 try {
 
     if (!document.querySelector('#backButton')) {
-        const backButtonStyle = `background-color: rgb(0 0 0 / 0%); width: 9px; margin: -43px 8px 8px 228px; -webkit-app-region: no-drag;`;
+        const backButtonStyle = `position: absolute; left: 0; float: left; background-color: rgb(0 0 0 / 0%); width: 9px; margin: 15px 0px 0px calc(var(--web-navigation-width) + 15px); cursor: pointer; z-index: 1000;`;
 
-        document.getElementsByClassName('loading-inner')[0].insertAdjacentHTML("afterbegin", `
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.getElementById('web-main').insertAdjacentHTML("afterbegin", `
             <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
                 <img src="https://beta.music.apple.com/assets/shelf/paddle-dark.svg" alt="nope">
             </div>
         `);
+        } else {
+            document.getElementById('web-main').insertAdjacentHTML("afterbegin", `
+            <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
+                <img src="https://beta.music.apple.com/assets/shelf/paddle-default.svg" alt="nope">
+            </div>
+        `);
+        }
+
     }
 
 } catch (e) {
