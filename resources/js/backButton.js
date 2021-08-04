fe8e9e60da9a22d7f55a1888e1d7a5d597b7fc4e
Core <64542347+coredev-uk@users.noreply.github.com>
2021-08-04 15:40:43 +0100

    Back button looks nicer and is now in the correct position at all times appearing above all elements.

--- resources/js/backButton.js
+++ resources/js/backButton.js
@@ -1,18 +1,34 @@
 try {
 
     if (!document.querySelector('#backButton')) {
-        const backButtonStyle = `position: absolute; left: 0; float: left; background-color: rgb(0 0 0 / 0%); width: 9px; margin: 15px 0px 0px calc(var(--web-navigation-width) + 15px); cursor: pointer; z-index: 1000;`;
+        const backButtonStyle = `
+        position: absolute; 
+        left: calc(var(--web-navigation-width) + 20px); 
+        top: 70px;
+        width: 35px;
+        height: 30px;
+        cursor: pointer; 
+        z-index: 10000; 
+        background-color: var(--sidebar); 
+        padding: 5px 10px 5px 10px;
+        border-radius: 5px;`;
+
+        const backButtonImageStyle = `
+        display: block;
+        width: 100%;
+        height: 100%;
+        `;
 
         if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
             document.getElementById('web-main').insertAdjacentHTML("afterbegin", `
             <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
-                <img src="https://beta.music.apple.com/assets/shelf/paddle-dark.svg" alt="nope">
+                <img src="https://beta.music.apple.com/assets/shelf/paddle-dark.svg" alt="Back Button" style="${backButtonImageStyle}">
             </div>
         `);
         } else {
             document.getElementById('web-main').insertAdjacentHTML("afterbegin", `
             <div id="backButton" onclick="ipcRenderer.send('back');" style="${backButtonStyle}">
-                <img src="https://beta.music.apple.com/assets/shelf/paddle-default.svg" alt="nope">
+                <img src="https://beta.music.apple.com/assets/shelf/paddle-default.svg" alt="Back Button" style="${backButtonImageStyle}">
             </div>
         `);
         }
