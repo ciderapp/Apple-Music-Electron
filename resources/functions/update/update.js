6a649fe705d88f43143c60ea2730a22ce43a7200
cryptofyre <cryptofyre@protonmail.com>
2021-08-15 10:47:02 -0500

    pls

--- resources/functions/update/update.js
+++ resources/functions/update/update.js
@@ -1,16 +1,53 @@
-const {autoUpdater} = require("electron-updater");
-const {Notification} = require('electron');
+const { autoUpdater } = require('electron-updater')
+const { app, dialog, Notification } = require('electron')
 const pjson = require('../../../package.json')
+const {Analytics} = require("../analytics/sentry");
+Analytics.init()
 
-module.exports = {
-    checkUpdates: function() {
-        autoUpdater.checkForUpdatesAndNotify().then(r => {
-            if (r && pjson !== r.updateInfo.version) {
-                new Notification({
-                    title: "Apple Music",
-                    body: `New Update available at version ${r.updateInfo.version}.`
-                }).show()
-            }
-        });
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+exports.checkUpdates = function (manual) {
+
+    if (app.preferences.value('advanced.autoUpdaterBetaBuilds').includes(true)) {
+        autoUpdater.allowPrerelease = true
+        autoUpdater.allowDowngrade = false
     }
+
+    autoUpdater.checkForUpdates()
+
+    autoUpdater.on('update-not-available', () => {
+        if (manual === true) {
+            let bodyVer = "You are on the latest version. " + `(v${pjson.version})`
+            new Notification({title: "Apple Music", body: bodyVer}).show()
+        }
+    })
+
+    autoUpdater.on('download-progress', (progress) => {
+        let convertedProgress = parseFloat(progress);
+        app.win.setProgressBar(convertedProgress)
+    })
+
+    autoUpdater.on('update-downloaded', () => {
+        console.log('New version downloaded. Starting user prompt.');
+
+        if (process.env.NODE_ENV === 'production') {
+            dialog.showMessageBox({
+                type: 'info',
+                title: 'Updates Available',
+                message: `Update was found and downloaded, would you like to install the update now?`,
+                buttons: ['Sure', 'No']
+            }, (buttonIndex) => {
+                if (buttonIndex === 0) {
+                    const isSilent = true;
+                    const isForceRunAfter = true;
+                    autoUpdater.quitAndInstall(isSilent, isForceRunAfter);
+                } else {
+                    updater.enabled = true
+                    updater = null
+                }
+            })
+        }
+
+    })
 }
