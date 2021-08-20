7c60083b55431b19320a58539fe82c339847aaae
Core <64542347+coredev-uk@users.noreply.github.com>
2021-08-20 23:29:04 +0100

    Attempt to fix file permissions v2

--- resources/functions/init/Init-Theme.js
+++ resources/functions/init/Init-Theme.js
@@ -2,7 +2,6 @@ const {app, nativeTheme} = require('electron');
 const {resolve, join} = require('path');
 const {copySync} = require('fs-extra');
 const fs = require('fs');
-const chmodr = require('chmodr');
 const {Analytics} = require("../analytics/sentry");
 Analytics.init()
 
@@ -30,37 +29,44 @@ exports.InitializeTheme = function () {
 
     // Make sure you can access the folder with the correct permissions
     console.log(`[InitializeTheme][access] Attempting to access '${app.userThemesPath}'`)
-    fs.access(app.userThemesPath, fs.constants.W_OK, err => {
 
-        if (err) { // File is not accessible
+    // Checks if the Folder is readable
+    try {
+        fs.accessSync(app.userThemesPath, fs.constants.F_OK)
+    } catch(err) {
+        console.error(err)
+        copySync(app.themesPath, app.userThemesPath)
+        console.log(`[InitializeTheme] [copyThemes] Themes copied to '${app.userThemesPath}'`)
+    }
 
-            // Copy the Files
-            if (!app.preferences.value('advanced.themeDevelopment').includes(true)) {
-                copySync(app.themesPath, app.userThemesPath)
-                console.log(`[InitializeTheme] [copyThemes] Themes copied to '${app.userThemesPath}'`)
-            }
+    // Copy the Files
+    if (app.preferences.value('advanced.themeDevelopment').includes(true)) {
+        copySync(app.themesPath, app.userThemesPath, {overwrite: true})
+        console.log(`[InitializeTheme] [copyThemes] Themes overwritten at '${app.userThemesPath}'`)
+    }
 
-            console.log(`[InitializeTheme][access] ${err}`)
+    // Save all the file names to array and log it
+    try {
+        app.themesList = fs.readdirSync(app.userThemesPath);
+        console.log('---------------------------------------------------------------------')
+        console.log(`Available Themes: ${app.themesList.join(', ')}`)
+        console.log('---------------------------------------------------------------------')
+    } catch (err) {
+        console.error(err)
+    }
 
-            chmodr(app.userThemesPath, 0o777, (_e) => { // Change permissions of the folder recursively
-                if (_e) {
-                    console.error(`[InitializeTheme][chmodr] ${_e}`)
-                } else {
-                    console.log('[InitializeTheme][chmodr] Theme folder permissions set.');
+    console.log('[InitializeTheme][access] Checking permissions...')
+
+    // Check the Permissions
+    if (app.themesList) {
+        app.themesList.forEach((value, _index) => {
+            fs.readFile(join(app.userThemesPath, value), "utf-8", function (error) {
+                if (error) {
+                    fs.chmodSync(join(app.userThemesPath, value), fs.constants.S_IRUSR | fs.constants.S_IWUSR);
+                    console.log(`[InitializeTheme][access] Setting permissions of '${join(app.userThemesPath, value)}'`)
                 }
-            });
+            })
+        })
+    }
 
-        } else {
-            console.log(`[InitializeTheme][access] Successfully able to access '${app.userThemesPath}'`)
-
-            try {
-                if (!app.preferences.value('advanced.themeDevelopment').includes(true)) {
-                    copySync(app.themesPath, app.userThemesPath, {overwrite: true})
-                    console.log(`[InitializeTheme] [copyThemes] Themes copied to '${app.userThemesPath}'`)
-                }
-            } catch (_err) {
-                console.log(_err)
-            }
-        }
-    });
 }
