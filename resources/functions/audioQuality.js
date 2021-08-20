3be80968b5499eea92f729fb53fed6d039e8a565
Fire <elijahklaumann@gmail.com>
2021-08-20 17:43:33 +0000

    Push some more code.

--- resources/functions/audioQuality.js
+++ resources/functions/audioQuality.js
@@ -2,14 +2,17 @@ const { app, BrowserWindow } = require('electron')
 const {Analytics} = require("./analytics/sentry");
 Analytics.init()
 
-exports.audioQuality = {
-    if (app.preferences.value('general.audioQuality').includes("auto")) {
-        console.log("[AudioQuality] Audio Quality has been set to automatic.")
-    } else {
-        if (app.preferences.value('general.audioQuality').includes("high")) {
-            app.win.webContents.executeJavaScript("").then(() => console.log(`[AudioQuality] Audio Quality has been set to High.`));
+exports.audioQuality = function() {
+    function set() {
+        if (app.preferences.value('general.audioQuality') === 'auto') {
+            console.log("[Audio] Audio Quality has been set to automatic.")
+        } else if (app.preferences.value('general.audioQuality') === 'high') {
+            console.log("[Audio] User has requested the High preset. Contacting MusicKit to fulfill.")
+            app.win.webContents.executeJavaScript("MusicKit.getInstance().Quality = HIGH").then(console.log("[Audio] Audio Quality has been set to High."))
+        } else if (app.preferences.value('general.audioQuality') === 'standard') {
+            console.log("[Audio] User has requested the Standard preset. Contacting MusicKit to fulfill.")
+            app.win.webContents.executeJavaScript("MusicKit.getInstance().Quality = NORMAL")
         }
     }
 }
-
-// still unfinished. been busy so commiting for now, shouldn't effect app runtime.
+// still unfinished. been busy so commiting for now, shouldn't effect app runtime. (this time)
