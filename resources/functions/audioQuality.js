23483348437e914cfe93b45641abb8bf57569310
cryptofyre <cryptofyre@protonmail.com>
2021-08-21 10:55:41 -0500

    adjust audio stuff

--- resources/functions/audioQuality.js
+++ resources/functions/audioQuality.js
@@ -13,11 +13,11 @@ exports.audioQuality = function() {
             console.log("[Audio] Audio Quality has been set to automatic.")
         } else if (app.preferences.value('general.audioQuality') === 'high') {
             console.log("[Audio] User has requested the High preset. Contacting MusicKit to fulfill.")
-            app.win.webContents.executeJavaScript("MusicKit.getInstance().bitrate = 256")
+            app.win.webContents.executeJavaScript("MusicKit.PlaybackBitrate = 256")
             console.log("[Audio] Audio Quality has been set to High (256kbps).")
         } else if (app.preferences.value('general.audioQuality') === 'normal') {
             console.log("[Audio] User has requested the Normal preset. Contacting MusicKit to fulfill.")
-            app.win.webContents.executeJavaScript("MusicKit.getInstance().bitrate = 64")
+            app.win.webContents.executeJavaScript("MusicKit.PlaybackBitrate = 64")
             console.log("[Audio] Audio Quality has been set to Normal (64kbps).")
         }
 }
