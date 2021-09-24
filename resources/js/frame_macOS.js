c6778709010af698b6b20df3b64fffe8a507d80c
Core <64542347+coredev-uk@users.noreply.github.com>
2021-09-24 22:31:22 +0100

    Small fix to override properties on lower sizes

--- resources/js/frame_macOS.js
+++ resources/js/frame_macOS.js
@@ -2,8 +2,8 @@ try {
     if (document.getElementById('web-navigation-search-box') && !document.querySelector('.web-nav-window-controls')) {
 
         document.getElementById('web-navigation-search-box').insertAdjacentHTML('beforebegin', `
-        <div class="web-nav-window-controls-outer" style="width: 100%; height: 55px; -webkit-app-region: no-drag; background-color: transparent !important; -webkit-user-select: none; padding-left: 3px; padding-top: 3px">
-            <div class="web-nav-window-controls" style="width: 100%; height: 100%; -webkit-app-region: drag;">
+        <div class="web-nav-window-controls-outer" style="width: 100%; height: 55px; -webkit-app-region: no-drag; background-color: transparent !important; -webkit-user-select: none; padding-left: 2px; padding-top: 2px;">
+            <div class="web-nav-window-controls" style="-webkit-app-region: drag; position: relative; height: 100%; width: 100%; display: flex; padding: 10px 0 0 10px;">
                 <span id="close" onclick="ipcRenderer.send('close')"></span>
                 <span id="minimize" onclick="ipcRenderer.send('minimize')"></span>
                 <span id="maximize" onclick="ipcRenderer.send('maximize')"></span>
