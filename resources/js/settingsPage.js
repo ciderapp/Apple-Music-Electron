ab1d60a975bfcf5c731d869c32205420a54897ea
CoreDev-UK <64542347+coredev-uk@users.noreply.github.com>
2021-09-27 14:20:18 +0100

    Initial Implementation

--- resources/js/settingsPage.js
+++ resources/js/settingsPage.js
@@ -5,7 +5,23 @@ try {
     };
 
     if (matchRuleShort(window.location.href, '*account/settings*')) {
-        document.getElementsByClassName('loading-inner commerce-iframe-wrapper')[0].innerHTML = ''
+        document.getElementsByClassName('loading-inner commerce-iframe-wrapper')[0].innerHTML = `
+            <div class="application-preferences">
+                <div class="app-prefs-section app-prefs-general">
+                    <span class="app-prefs-title">General Settings</span>
+                    <ul class="settings-list">
+                    <li class="app-prefs-togg">
+                        <span>Toggle Slider</span>
+                        <label class="switch">
+                            <input type="checkbox" checked>
+                            <span class="slider round"></span>
+                        </label>
+                    </li>
+                    <li class="app-prefs-dropdown">List Elements</li>
+                    </ul>
+                </div>
+            </div>
+        `
         /*Here we create the settings menu and add onclicks for all elements and use electron-store*/
     }
 } catch (e) {
