a317a9d18d6e79eef092651b652660b0c8a7d4d8
Nicolai Van der Storm <nicolai@nvds.be>
2021-10-27 23:45:45 +0200

    added const and used whol attributes compare

--- resources/functions/media/lastfm.js
+++ resources/functions/media/lastfm.js
@@ -60,7 +60,7 @@ const lfm = {
 
     scrobbleSong: async function (attributes) {
         await new Promise(resolve => setTimeout(resolve, app.cfg.get('general.lastfmScrobbleDelay') * 1000));
-        currentAttributes = app.media;
+        const currentAttributes = app.media;
         
         if (!app.lastfm || app.lastfm.cachedAttributes === attributes || app.cfg.get('general.incognitoMode')) {
             return
@@ -70,10 +70,7 @@ const lfm = {
             if (app.lastfm.cachedAttributes.playParams.id === attributes.playParams.id) return;
         }
 
-        currentSong = currentAttributes.name + ' - ' + lfm.filterArtistName(currentAttributes.artistName)
-        receivedSong = attributes.name + ' - ' + lfm.filterArtistName(attributes.artistName)
-
-        if (currentAttributes.status && currentSong === receivedSong) {
+        if (currentAttributes.status && currentAttributes === attributes) {
             if (fs.existsSync(sessionPath)) {
                 // Scrobble playing song.
                 if (attributes.status === true) {
